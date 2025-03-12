556a7d2c29fbcea8f0be050159192a064cada9e5
fjortegan <fjortegan@gmail.com>
2025-03-12 09:10:41 +0100

    loot

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject chestPrefab;
     [SerializeField] private int numberOfChests;
     
-    private Vector3 spawnPosition = new Vector3(45.5f, 2.25f, 49.5f);
-    private int columnSize = 48;
+    [SerializeField] private Vector3 spawnPosition = new Vector3(45.5f, 2.25f, 49.5f);
+    [SerializeField] private int rowSize = 48;
+    [SerializeField] private float chestWidth = 2.0f;
+    [SerializeField] private float chestHeight = 2.0f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         for (int i = 0; i < numberOfChests; i++)
         {
-            Instantiate(chestPrefab, spawnPosition + new Vector3(-(i%columnSize)*2, 0, -Mathf.Floor(i/columnSize)*2), Quaternion.identity);
+            Instantiate(chestPrefab, 
+                 spawnPosition + new Vector3(-(i%rowSize)*chestWidth, 0, -Mathf.Floor(i/rowSize)*chestHeight),
+                        Quaternion.identity);
         }
     }
 
